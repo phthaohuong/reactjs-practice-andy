a5194d160c3321c0572c7e6b92b3cde5b0a2b1a6
phthaohuong <thaohuongtqt1603@gmail.com>
2020-10-05 14:30:43 +0700

    'setup_redux_store'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./redux/rootReducer.js";
 
 // Import Bootstrap
 import "../node_modules/jquery/dist/jquery.min.js";
@@ -11,13 +11,9 @@ import "../node_modules/popper.js/dist/umd/popper.min.js";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
